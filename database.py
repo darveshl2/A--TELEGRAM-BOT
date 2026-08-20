bb36842290f64b2e80044e8c42cd18662bd9827d
darveshl2 <qamarovsuhayb@gmail.com>
2026-08-20 17:40:28 +0300

    Update database path to use BASE_DIR

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
+import os
 import sqlite3
 from datetime import datetime
-from config import DATABASE
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE = os.path.join(BASE_DIR, "bot.db")
 
 
 def get_connection():
